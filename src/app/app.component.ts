e2373ce82a18fc4259be90e81a26a2806833abf3
Maik Kingma <m.kingma2@dictu.nl>
2023-03-30 16:59:50 +0200

    updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {OidcSecurityService} from "angular-auth-oidc-client";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular-oidc-test';
+  constructor(public oidcSecurityService: OidcSecurityService) {}
+
+  ngOnInit() {
+    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData, accessToken, idToken }) => {
+      console.log('app authenticated', isAuthenticated);
+      if(!isAuthenticated) {
+        this.login();
+      }
+      console.log(`Current access token is '${accessToken}'`);
+    });
+  }
+
+  login() {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout() {
+    this.oidcSecurityService.logoff().subscribe((result) => console.log(result));
+  }
+
 }
